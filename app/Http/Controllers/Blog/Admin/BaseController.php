4f6821750e34724e6533a35d2210015634b465c9
vadim <mvadyan@gmail.com>
2020-12-25 07:36:31 +0200

    category add repository

--- app/Http/Controllers/Blog/Admin/BaseController.php
+++ app/Http/Controllers/Blog/Admin/BaseController.php
@@ -5,7 +5,18 @@ namespace App\Http\Controllers\Blog\Admin;
 use App\Http\Controllers\Blog\BaseController as GuestController;
 use Illuminate\Http\Request;
 
+/**
+ * Class BaseController
+ * @package App\Http\Controllers\Blog\Admin
+ */
 abstract class BaseController extends GuestController
 {
+
+    /**
+     * BaseController constructor.
+     */
+    public function __construct()
+    {
+    }
     //
 }
